61afda7eb8fb1b9730d40f76ce543744c5dd7188
romaing <rocambille@gmail.com>
2022-05-12 20:41:55 +0200

    refactored html structure

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 function getValue() {
-    let random = Math.floor(Math.random() * 2)
-    console.log(random)
-    if (random === 0){
-        alert ("Vrai ! 	✅")
-        }
-    else alert ("Faux ! ❌")     
-    }
+  let random = Math.floor(Math.random() * 2);
+  console.log(random);
+  if (random === 0) {
+    alert("Vrai ! 	✅");
+  } else alert("Faux ! ❌");
+}
 
 if (window.matchMedia("(min-width: 1024px)").matches) {
-
-    document.querySelectorAll(".courses-details").forEach(
-        (coursesDetail) => {
-            coursesDetail.setAttribute("open", "")
-        }
-    );
+  document.querySelectorAll(".courses-details").forEach((coursesDetail) => {
+    coursesDetail.setAttribute("open", "");
+  });
 }
+
+let keyDownListener = null;
+
+document.querySelectorAll(".modal").forEach((modal) => {
+  modal.addEventListener("toggle", (event) => {
+    if (event.target.open) {
+      keyDownListener = ({ key }) => {
+        if (key === "Escape") {
+          event.target.open = false;
+        }
+      };
+
+      window.addEventListener("keydown", keyDownListener);
+    } else {
+      window.removeEventListener("keydown", keyDownListener);
+      keyDownListener = null;
+    }
+  });
+});
